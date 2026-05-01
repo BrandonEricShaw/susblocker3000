b2ca725b322a2f39961dbd3bda0f32bcebf76289
BrandonShaw <shaw.brandon64@gmail.com>
2026-04-30 23:44:59 -0400

    Changed to observer based blocking, added content script to block ads on youtube, added permissions for content script, updated manifest file

--- youtubeBlocking.js
+++ youtubeBlocking.js
@@ -1,12 +1,14 @@
-function skipAd() {
-    //Consts
+//Ident
+let observer;
 
-    //Auto Skip Button selector
-    const skipButton = document.querySelector('.ytp-ad-skip-button.ytp-button');
-    //Overlay Selector
-    const adOverlay = document.querySelector('.ytp-ad-player-overlay');
-    //Video selector
+//Functions
+function skipAd() {
+
+    //Consts
+    const isAdShowing = document.querySelector('.ad-showing');
     const video = document.querySelector('video');
+    const skipButton = document.querySelector('.ytp-ad-skip-button, .ytp-ad-skip-button-modern');
+    const adOverlay = document.querySelectorAll('.ytp-ad-overlay-container .ytp-ad-player-overlay');
 
     //Calcs
 
@@ -15,17 +17,43 @@ function skipAd() {
         skipButton.click();
     }
     
-    //Remove Ad Overlay
-    if (adOverlay) {
-        adOverlay.remove();
+    //Mute Ads & Skip to end
+    if (isAdShowing && video) {
+        video.muted = true;
+        //try and instant jump to the end of the ad
+        try {
+            if (video.duration) {
+                video.currentTime = video.duration;
+            }
+        //Write error to console
+        } catch (error) {
+            console.error('Error skipping ad:', error);
+        }
     }
 
-    //Mute Ads & Skip to end
-    if (video && document.querySelector('.ad-showing')) {
-        video.muted = true;
-        video.currentTime = video.duration || 0;
-    }
+    //Remove Ad Overlay
+    adOverlay.forEach(overlay => overlay.remove());
+
 }
 
-//Run every 500ms
-setInterval(skipAd, 500);
+function startObserver() {
+
+    //Observe for changes in the DOM to try and dected an Ad starts
+    if (observer) {
+        observer.disconnect();
+    }
+    observer = new MutationObserver(() => {
+        childList = true,
+        subtree = true
+    });
+    //Run Once
+    skipAd();
+}
+
+function init() {
+    startObserver();
+}
+
+//Event Listeners; Window for youtube which fires on nvaigation, Document for the rest of the site which fires on load
+window.addEventListener('yt-navigate-finish', init);
+document.addEventListener
