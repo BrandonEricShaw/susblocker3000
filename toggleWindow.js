2a0b7d6196f4ecd668e288f4207fd4c23ab71902
Brandon Shaw <shaw.brandon64@gmail.com>
2026-05-19 22:31:14 -0400

    Added comments

--- toggleWindow.js
+++ toggleWindow.js
@@ -1,16 +1,20 @@
+//Const
 const toggleButton = document.getElementById("toggleButton");
 
+//Change Buttons State
 function updateButton(enabled) {
   toggleButton.textContent = enabled
     ? "Disable Susblocker"
     : "Enable Susblocker";
 }
 
+//Update Button, Toggle Capability
 chrome.storage.local.get(["enabled"], (result) => {
   const enabled = result.enabled !== false;
   updateButton(enabled);
 });
 
+//Button Event Listener
 toggleButton.addEventListener("click", () => {
   chrome.storage.local.get(["enabled"], (result) => {
     const enabled = result.enabled !== false;
